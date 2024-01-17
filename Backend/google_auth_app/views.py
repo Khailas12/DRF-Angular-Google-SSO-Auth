425f11a95d01c26dfd4ee4ff052fd1ffb9c68a10
Khailas12 <khailas303@gmail.com>
2024-01-17 14:52:31 +0530

    unused imports removed

--- Backend/google_auth_app/views.py
+++ Backend/google_auth_app/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from google.auth.transport import requests
 from google.oauth2 import id_token
 import os
-from rest_framework import status, response
 
 load_dotenv()
 
